5749bff82748ee42b5e8141e97da6da81562a99d
Jon Preece <jonpreece@Jons-MacBook-Pro.local>
2018-11-13 19:54:00 +0000

    Initial commit

---
+++ BuyMyHouse-API/Controllers/ListingsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BuyMyHouse_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListingsController : ControllerBase
+    {
+        private string _key;
+
+        public ListingsController()
+        {
+            _key = Environment.GetEnvironmentVariable("API_KEY");
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            var client = new HttpClient();
+            var url = $"https://api.zoopla.co.uk/api/v1/property_listings.json?radius=3&postcode=M3&area=Media%20City&api_key={_key}";
+            Console.WriteLine(url);
+            return await client.GetStringAsync(url);
+        }
+    }
+}
